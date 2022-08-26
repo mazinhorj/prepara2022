a4dfde2afccad1a9abdca93e5ccaa36e6b034278
hc2600 <heavy2600@gmail.com>
2022-08-26 06:37:36 -0300

    estrutura inicial com bootstrap

---
+++ garbage/padrao.php
@@ -0,0 +1,20 @@
+<?php 
+    require_once("conexao/Db.class.php");
+	$db = new Db();
+	
+	//Queries: $db->query(string)
+	
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+       <meta charset="utf-8"/>
+       <title>PREPARA</title>
+	   
+   </head>
+   <body>
+       
+	   
+   </body>
+</html>
